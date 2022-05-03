8cc0fbf29aabec77ed1fdf8c09d828c708c36a0c
Amit Yadav <amit15885@gmail.com>
2022-05-03 20:12:27 +0530

    rm

--- AYPod/AYPod.swift
+++ AYPod/AYPod.swift
@@ -5,12 +5,13 @@
 //  Created by Amit Yadav on 03/05/22.
 //
 
-import ay_test_sdk
+//import ay_test_sdk
 
 public class AYPod {
     
     public static func test () -> Int {
         print("test ok")
-        return AYTest.rollDice()
+        //return AYTest.rollDice()
+        return 1
     }
 }
