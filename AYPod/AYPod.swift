e549c5986a0ba3291c4ae726d4db4832a11808af
Amit Yadav <amit15885@gmail.com>
2022-05-03 17:15:10 +0530

    clean

--- AYPod/AYPod.swift
+++ AYPod/AYPod.swift
@@ -5,13 +5,9 @@
 //  Created by Amit Yadav on 03/05/22.
 //
 
-//import ay_test_sdk
-
 public class AYPod {
     
     public static func test () {
         print("test ok")
-        // print( AYTest.rollDice() )
     }
-    
 }
