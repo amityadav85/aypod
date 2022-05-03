2c1a8b1285164ddffbe718f71886e21d8e2b5e82
Amit Yadav <amit15885@gmail.com>
2022-05-03 19:27:08 +0530

    added framework

---
+++ AYPod/ay_test_sdk.xcframework/ios-arm64/ay_test_sdk.framework/Headers/ay_test_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ay_test_sdk.h
+//  ay-test-sdk
+//
+//  Created by Amit Yadav on 03/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ay_test_sdk.
+FOUNDATION_EXPORT double ay_test_sdkVersionNumber;
+
+//! Project version string for ay_test_sdk.
+FOUNDATION_EXPORT const unsigned char ay_test_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ay_test_sdk/PublicHeader.h>
+
+
